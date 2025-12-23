e53549f2a2f5d58f49e6951d243cbbfc1a465bf1
lanxi <1748440980@qq.com>
2025-12-23 23:40:26 +0800

    <feature>: log |> 日志启用异步输出功能
    依赖提交：
    	<ref>: easylogger源码ElogErrCode增加ELOG_HAS_ERR返回值
    	fba6f3f268b0d1ec8be4f4c4c343b7c498e64039

--- app/main.c
+++ app/main.c
@@ -25,6 +25,7 @@ static void main_init(void *param)
     elog_set_fmt(ELOG_LVL_INFO, ELOG_FMT_LVL | ELOG_FMT_TAG | ELOG_FMT_TIME);
     elog_set_fmt(ELOG_LVL_DEBUG, ELOG_FMT_LVL | ELOG_FMT_TAG);
     elog_set_fmt(ELOG_LVL_VERBOSE, ELOG_FMT_LVL | ELOG_FMT_TAG);
+    elog_async_enabled(true);
     elog_start();
 
     welcome_page_display();
--- third_lib/easylogger/port/elog_port.c
+++ third_lib/easylogger/port/elog_port.c
@@ -3,19 +3,69 @@
 #include "usart.h"
 #include "rtc.h"
 #include "FreeRTOS.h"
+#include "task.h"
 #include "semphr.h"
 
+static bool elog_inited;
 static SemaphoreHandle_t elog_mutex;
+static SemaphoreHandle_t elog_semphr;
+
+static void async_output(void *arg);
+
 ElogErrCode elog_port_init(void)
 {
+    BaseType_t result;
+
+    if (elog_inited)
+    {
+        return ELOG_NO_ERR;
+    }
+
     elog_mutex = xSemaphoreCreateMutex();
+    if (elog_mutex == NULL)
+    {
+        printf("Error: EasyLogger mutex create failed!\r\n");
+        goto err1;
+    }
+    elog_semphr = xSemaphoreCreateBinary();
+    if (elog_semphr == NULL)
+    {
+        printf("Error: EasyLogger semaphore create failed!\r\n");
+        goto err2;
+    }
+
+    result = xTaskCreate(async_output, "elog", 1024, NULL, tskIDLE_PRIORITY + 1, NULL);
+    if (result != pdPASS)
+    {
+        printf("Error: EasyLogger async output task create failed!\r\n");
+        goto err3;
+    }
+
+    elog_inited = true;
     return ELOG_NO_ERR;
+
+err3:
+    vSemaphoreDelete(elog_semphr);
+    elog_semphr = NULL;
+err2:
+    vSemaphoreDelete(elog_mutex);
+    elog_mutex = NULL;
+err1:
+    return ELOG_HAS_ERR;
 }
 
 void elog_port_deinit(void)
 {
-    vSemaphoreDelete(elog_mutex);
-    elog_mutex = NULL;
+    if (elog_semphr != NULL)
+    {
+        vSemaphoreDelete(elog_semphr);
+        elog_semphr = NULL;
+    }
+    if (elog_mutex != NULL)
+    {
+        vSemaphoreDelete(elog_mutex);
+        elog_mutex = NULL;
+    }
 }
 
 void elog_port_output(const char *log, size_t size)
@@ -74,5 +124,36 @@ const char *elog_port_get_t_info(void)
     else
     {
         return "none";
+    }
+}
+
+void elog_async_output_notice(void)
+{
+    if (elog_semphr != NULL)
+    xSemaphoreGive(elog_semphr);
+}
+
+static void async_output(void *arg)
+{
+    size_t log_size = 0;
+    char log_buf[ELOG_LINE_BUF_SIZE];
+
+    while(true)
+    {
+        xSemaphoreTake(elog_semphr, portMAX_DELAY);
+
+        do
+        {
+#ifdef ELOG_ASYNC_LINE_OUTPUT
+            log_size = elog_async_get_line_log(log_buf, ELOG_LINE_BUF_SIZE);
+#else
+            log_size = elog_async_get_log(log_buf, ELOG_LINE_BUF_SIZE);
+#endif
+
+            if (log_size > 0)
+            {
+                elog_port_output(log_buf, log_size);
+            }
+        } while (log_size > 0);
     }
 }
