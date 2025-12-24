548840fbe2250ee0f49d1fefb750ab11557aef23
lanxi <1748440980@qq.com>
2025-12-25 01:55:41 +0800

    <elog>: 封装elog初始化代码

---
+++ app/component/logger_init.c
@@ -0,0 +1,14 @@
+#include "elog.h"
+
+void logger_init(void)
+{
+    elog_init();
+    elog_set_fmt(ELOG_LVL_ASSERT, ELOG_FMT_ALL);
+    elog_set_fmt(ELOG_LVL_ERROR, ELOG_FMT_ALL);
+    elog_set_fmt(ELOG_LVL_WARN, ELOG_FMT_LVL | ELOG_FMT_TAG | ELOG_FMT_TIME | ELOG_FMT_T_INFO);
+    elog_set_fmt(ELOG_LVL_INFO, ELOG_FMT_LVL | ELOG_FMT_TAG | ELOG_FMT_TIME);
+    elog_set_fmt(ELOG_LVL_DEBUG, ELOG_FMT_LVL | ELOG_FMT_TAG);
+    elog_set_fmt(ELOG_LVL_VERBOSE, ELOG_FMT_LVL | ELOG_FMT_TAG);
+    elog_async_enabled(true);
+    elog_start();
+}
--- app/main.c
+++ app/main.c
@@ -6,27 +6,19 @@
 #include "wifi.h"
 #include "page.h"
 #include "ui.h"
-#include "elog.h"
 #include "workqueue.h"
 
 extern void board_init(void);
 extern void board_lowlevel_init(void);
+extern void logger_init();
 
 static void main_init(void *param)
 {
     board_init();
 
-    ui_init();
+    logger_init();
 
-    elog_init();
-    elog_set_fmt(ELOG_LVL_ASSERT, ELOG_FMT_ALL);
-    elog_set_fmt(ELOG_LVL_ERROR, ELOG_FMT_ALL);
-    elog_set_fmt(ELOG_LVL_WARN, ELOG_FMT_LVL | ELOG_FMT_TAG | ELOG_FMT_TIME | ELOG_FMT_T_INFO);
-    elog_set_fmt(ELOG_LVL_INFO, ELOG_FMT_LVL | ELOG_FMT_TAG | ELOG_FMT_TIME);
-    elog_set_fmt(ELOG_LVL_DEBUG, ELOG_FMT_LVL | ELOG_FMT_TAG);
-    elog_set_fmt(ELOG_LVL_VERBOSE, ELOG_FMT_LVL | ELOG_FMT_TAG);
-    elog_async_enabled(true);
-    elog_start();
+    ui_init();
 
     welcome_page_display();
 
